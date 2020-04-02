f41736dd463842d8c8dc9eb442b9d3bb771bcb05
ptyagicodecamp <ptyagicodecamp@gmail.com>
2020-04-02 16:01:05 -0700

    addeing runtimeType

---
+++ src/runtimeType.dart
@@ -0,0 +1,13 @@
+void main() {
+  var myNumber = 1;
+  print("Type of myNumber: ${myNumber.runtimeType}");
+
+  var myString = "This is String";
+  print("Type of myString: ${myString.runtimeType}");
+
+  var myDouble = 1.0;
+  print("Type of myDouble: ${myDouble.runtimeType}");
+
+  var myList = [1, 2, 3];
+  print("Type of myList: ${myList.runtimeType}");
+}
