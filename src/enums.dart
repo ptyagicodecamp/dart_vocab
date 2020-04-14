3259f1d550bb887142025b797426deb9e2260d94
ptyagicodecamp <ptyagicodecamp@gmail.com>
2020-04-14 14:43:53 -0700

    enums

--- src/enums.dart
+++ src/enums.dart
@@ -1,6 +1,4 @@
-//`enum` is a keyword to declare enumerated (a.k.a enums) types.
-//Enums is like a class to represent a fixed number of constant values.
-//Enums are added as an experimental feature in Dart 1.8 release: https://news.dartlang.org/2014/11/dart-18-library-improvements-and.html
+//Declaring enums
 enum Weather {
   SUNNY,
   CLOUDY,
@@ -8,45 +6,77 @@ enum Weather {
 }
 
 void main() {
+  mainExtension();
+}
+
+//Using constants to display weather information
+void mainSwitchConstants() {
   const SUNNY = 'Sunny';
   const CLOUDY = 'Cloudy';
   const RAINY = 'Rainy';
 
-  //#1. Difference between const and enums
-  //switch doesn't complain for const, but raise error for enums
-  var choice = CLOUDY;
+  //#1. Switch doesn't complain for const, but raise error for enums
+  var choice = SUNNY;
   switch (choice) {
-    case CLOUDY:
-      print("Cloudy today");
+    case SUNNY:
+      print("Sunny weather today");
       break;
   }
+}
 
+//Using Enums to display weather information
+void mainSwitchEnums() {
+  var weather = Weather.SUNNY;
+  //Following code will complain about
+  // not including other types of weather
+  //OR use default
+  switch (weather) {
+    case Weather.SUNNY:
+      print("Sunny weather today!");
+      break;
+    case Weather.CLOUDY:
+      print("Cloudy today!");
+      break;
+    case Weather.RAINY:
+      print("Rainy and gloomy weather.");
+      break;
+  }
+}
+
+//Only default case. No compilation issue
+void mainSwitchEnumsDefault() {
   var weather = Weather.SUNNY;
 
-//  switch (weather) {
-//    case Weather.SUNNY:
-//      print("Sunny weather today!");
-//      break;
-//  }
+  switch (weather) {
+    default:
+      print("Current weather:${weather}");
+  }
+}
 
+void mainIterating() {
   //#2. enums can iterate on all types at once.
   //No need to create a list of consts
-  //Weather.values.forEach((w) => print(w));
+  Weather.values.forEach((w) => print(w));
+}
 
-  //#3. Enum extensions
+void mainExtension() {
+  //#3. Enum extensions. Using extension method console
   Weather.values.forEach((w) => w.console());
 }
 
 extension WeatherExt on Weather {
-  static final weatherMap = {
+  //custom message for each weather type
+  static const weatherMap = {
     Weather.SUNNY: "What a lovely weather",
     Weather.CLOUDY: "Scattered showers predicted",
     Weather.RAINY: "Will be raining today",
   };
 
+  //prints enum index and custom message
   void console() {
     print("${this.index} ${this.about}");
   }
 
+  //about property returns the custom message
   String get about => weatherMap[this];
 }
