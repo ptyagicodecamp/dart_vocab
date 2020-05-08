f06154624a68489d1179ad99cdf7d97ba96ca041
ptyagicodecamp <ptyagicodecamp@gmail.com>
2020-05-07 23:49:07 -0700

    added libraries

---
+++ src/libraries/maths_part/part_main.dart
@@ -0,0 +1,11 @@
+import 'lib1.dart';
+
+void main() {
+  int num1 = 5;
+  int num2 = 2;
+
+  int sum = addition(num1, num2);
+  print("Sum of $num1 and $num2 is $sum");
+
+  print("is number even? ${isNumberEven(num2)}");
+}
