72ec4e1f7e76fdbed9f45917422930d8b00accc2
ptyagicodecamp <ptyagicodecamp@gmail.com>
2020-05-20 21:43:19 -0700

    added Futures examples

---
+++ src/futures/futures_await.dart
@@ -0,0 +1,18 @@
+//Entry point function
+void main() {
+  makeDataCall();
+}
+
+// Expensive function could be a function that takes
+// long time to process data and return results.
+// Assume this function takes long time to return in real-world
+String getExpansiveData() {
+  return "I'm expansive data";
+}
+
+// This is the asynchronous function that makes the expensive
+// data call and prints the results.
+Future<void> makeDataCall() async {
+  var data = await getExpansiveData();
+  print(data);
+}
